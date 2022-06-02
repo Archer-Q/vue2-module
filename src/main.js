0d243b03c8f10920d2ed5ce1c1e51179d76bff2c
Archer <9527.code@gmail.com>
2022-06-02 16:10:11 +0800

    安装依赖

--- src/main.js
+++ src/main.js
@@ -2,8 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入字体图标
+import './Icons';
+// 引入moment
+import moment from 'moment';
+Vue.prototype.$moment = moment;
+// 引入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
+// 引入echarts
+import * as echarts from 'echarts';
+Vue.prototype.$echarts = echarts;
+// 引入MD5加密插件
+import md5 from 'js-md5';
+Vue.prototype.$md5 = md5;
+// 引入js-cookie
+import Cookies from 'js-cookie';
 
-import './Icons'
+// 引入mathjs
+import * as math from 'mathjs';
+Vue.prototype.$math = math;
 
 Vue.config.productionTip = false
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <svg-icon icon-class="add"></svg-icon>
+    <el-button>111</el-button>
+    <div id="main" style="width: 600px;height:400px;"></div>
   </div>
 </template>
 <script>
@@ -11,11 +13,39 @@ export default {
       vue: 'Vue'
     }
   },
+  methods: {
+    drawChart() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById("main"));
+      // 指定图表的配置项和数据
+      let option = {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    }
+  },
   mounted() {
-    let url = process.env.NODE_ENV === 'production' ?  window.productionUrl : window.developmentUrl
-    console.log(process.env.NODE_ENV)
-    console.log(window.productionUrl)
-    console.log(window.developmentUrl)
+    console.log(this.$md5('密码'))
+    console.log(this.$math.add(2,3))
+    this.drawChart();
   }
 }
 </script>
